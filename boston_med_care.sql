e9f36dcc33f0fc585790acff95bf91813e952109
fazlur10 <sfazlurrahmanrs@gmail.com>
2024-12-17 11:11:16 +0300

    db updated

--- boston_med_care.sql
+++ boston_med_care.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2024 at 03:28 PM
+-- Generation Time: Dec 17, 2024 at 09:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
